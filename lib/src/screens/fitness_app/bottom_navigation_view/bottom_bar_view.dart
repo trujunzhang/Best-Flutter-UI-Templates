00791963b81c4edcdb9ea1e9e4262b92549b5ad6
djzhang <trujunzhang@gmail.com>
2020-05-05 09:53:46 +0800

    BottomBarView

--- lib/src/screens/fitness_app/bottom_navigation_view/bottom_bar_view.dart
+++ lib/src/screens/fitness_app/bottom_navigation_view/bottom_bar_view.dart
@@ -1,19 +1,16 @@
-import 'package:ieatta/src/screens/fitness_app/fitness_app_theme.dart';
-import 'package:ieatta/src/screens/fitness_app/bottom_navigation_view/tabIcon_data.dart';
-import 'package:ieatta/src/utils/hex_color.dart';
 import 'package:flutter/material.dart';
 
 import 'tabIcon_data.dart';
-import 'tab_clipper.dart';
-import 'tab_icons.dart';
+import 'tab_bg.dart';
+import 'tab_fg.dart';
 
 class BottomBarView extends StatefulWidget {
   const BottomBarView(
       {Key key, this.tabIconsList, this.changeIndex, this.addClick})
       : super(key: key);
 
-  final Function(int index) changeIndex;
   final Function addClick;
+  final Function(int index) changeIndex;
   final List<TabIconData> tabIconsList;
 
   @override
@@ -34,168 +31,20 @@ class _BottomBarViewState extends State<BottomBarView>
     super.initState();
   }
 
-  Widget buildBg() {
-    return AnimatedBuilder(
-      animation: animationController,
-      builder: (BuildContext context, Widget child) {
-        return Transform(
-          transform: Matrix4.translationValues(0.0, 0.0, 0.0),
-          child: PhysicalShape(
-            color: FitnessAppTheme.white,
-            elevation: 16.0,
-            clipper: TabClipper(
-                radius: Tween<double>(begin: 0.0, end: 1.0)
-                        .animate(CurvedAnimation(
-                            parent: animationController,
-                            curve: Curves.fastOutSlowIn))
-                        .value *
-                    38.0),
-            child: Column(
-              children: <Widget>[
-                SizedBox(
-                  height: 62,
-//                      height: 0,
-                  child: Padding(
-                    padding: const EdgeInsets.only(left: 8, right: 8, top: 4),
-                    child: Row(
-                      children: <Widget>[
-                        Expanded(
-                          child: TabIcons(
-                              tabIconData: widget.tabIconsList[0],
-                              removeAllSelect: () {
-                                setRemoveAllSelection(widget.tabIconsList[0]);
-                                widget.changeIndex(0);
-                              }),
-                        ),
-                        Expanded(
-                          child: TabIcons(
-                              tabIconData: widget.tabIconsList[1],
-                              removeAllSelect: () {
-                                setRemoveAllSelection(widget.tabIconsList[1]);
-                                widget.changeIndex(1);
-                              }),
-                        ),
-                        SizedBox(
-                          width: Tween<double>(begin: 0.0, end: 1.0)
-                                  .animate(CurvedAnimation(
-                                      parent: animationController,
-                                      curve: Curves.fastOutSlowIn))
-                                  .value *
-                              64.0,
-                        ),
-                        Expanded(
-                          child: TabIcons(
-                              tabIconData: widget.tabIconsList[2],
-                              removeAllSelect: () {
-                                setRemoveAllSelection(widget.tabIconsList[2]);
-                                widget.changeIndex(2);
-                              }),
-                        ),
-                        Expanded(
-                          child: TabIcons(
-                              tabIconData: widget.tabIconsList[3],
-                              removeAllSelect: () {
-                                setRemoveAllSelection(widget.tabIconsList[3]);
-                                widget.changeIndex(3);
-                              }),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-                SizedBox(
-                  height: MediaQuery.of(context).padding.bottom,
-                )
-              ],
-            ),
-          ),
-        );
-      },
-    );
-  }
-
-  Widget buildFg() {
-    return Padding(
-      padding: EdgeInsets.only(bottom: MediaQuery.of(context).padding.bottom),
-      child: SizedBox(
-        width: 38 * 2.0,
-        height: 38 + 62.0,
-        child: Container(
-          alignment: Alignment.topCenter,
-          color: Colors.transparent,
-          child: SizedBox(
-            width: 38 * 2.0,
-            height: 38 * 2.0,
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: ScaleTransition(
-                alignment: Alignment.center,
-                scale: Tween<double>(begin: 0.0, end: 1.0).animate(
-                    CurvedAnimation(
-                        parent: animationController,
-                        curve: Curves.fastOutSlowIn)),
-                child: Container(
-                  // alignment: Alignment.center,s
-                  decoration: BoxDecoration(
-                    color: FitnessAppTheme.nearlyDarkBlue,
-                    gradient: LinearGradient(colors: [
-                      FitnessAppTheme.nearlyDarkBlue,
-                      HexColor('#6A88E5'),
-                    ], begin: Alignment.topLeft, end: Alignment.bottomRight),
-                    shape: BoxShape.circle,
-                    boxShadow: <BoxShadow>[
-                      BoxShadow(
-                          color:
-                              FitnessAppTheme.nearlyDarkBlue.withOpacity(0.4),
-                          offset: const Offset(8.0, 16.0),
-                          blurRadius: 16.0),
-                    ],
-                  ),
-                  child: Material(
-                    color: Colors.transparent,
-                    child: InkWell(
-                      splashColor: Colors.white.withOpacity(0.1),
-                      highlightColor: Colors.transparent,
-                      focusColor: Colors.transparent,
-                      onTap: () {
-                        widget.addClick();
-                      },
-                      child: Icon(
-                        Icons.photo_camera,
-                        color: FitnessAppTheme.white,
-                        size: 32,
-                      ),
-                    ),
-                  ),
-                ),
-              ),
-            ),
-          ),
-        ),
-      ),
-    );
-  }
-
   @override
   Widget build(BuildContext context) {
     return Stack(
       alignment: AlignmentDirectional.bottomCenter,
       children: <Widget>[
-        buildBg(),
-        buildFg()
+        TabBg(
+            animationController: animationController,
+            tabIconsList: widget.tabIconsList,
+            changeIndex: widget.changeIndex),
+        TabFg(
+          animationController: animationController,
+          addClick: widget.addClick,
+        )
       ],
     );
-  }
-
-  void setRemoveAllSelection(TabIconData tabIconData) {
-    if (!mounted) return;
-    setState(() {
-      widget.tabIconsList.forEach((TabIconData tab) {
-        tab.isSelected = false;
-        if (tabIconData.index == tab.index) {
-          tab.isSelected = true;
-        }
-      });
-    });
   }
 }

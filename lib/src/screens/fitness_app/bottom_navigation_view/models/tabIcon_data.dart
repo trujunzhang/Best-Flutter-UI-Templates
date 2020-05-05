10f8a6d0a0ceac0bb888ecc56f3d342748b92f01
djzhang <trujunzhang@gmail.com>
2020-05-05 14:28:22 +0800

    TabIconData

--- lib/src/screens/fitness_app/bottom_navigation_view/models/tabIcon_data.dart
+++ lib/src/screens/fitness_app/bottom_navigation_view/models/tabIcon_data.dart
@@ -3,16 +3,12 @@ import 'package:flutter/material.dart';
 class TabIconData {
   TabIconData({
     this.icon,
-    this.imagePath = '',
     this.index = 0,
-    this.selectedImagePath = '',
     this.isSelected = false,
     this.animationController,
   });
 
   IconData icon;
-  String imagePath;
-  String selectedImagePath;
   bool isSelected;
   int index;
 
@@ -30,32 +26,24 @@ class TabIconData {
   static List<TabIconData> tabIconsList = <TabIconData>[
     TabIconData(
       icon: Icons.restaurant,
-      imagePath: 'assets/fitness_app/tab_1.png',
-      selectedImagePath: 'assets/fitness_app/tab_1s.png',
       index: 0,
       isSelected: true,
       animationController: null,
     ),
     TabIconData(
       icon: Icons.people,
-      imagePath: 'assets/fitness_app/tab_2.png',
-      selectedImagePath: 'assets/fitness_app/tab_2s.png',
       index: 1,
       isSelected: false,
       animationController: null,
     ),
     TabIconData(
       icon: Icons.rate_review,
-      imagePath: 'assets/fitness_app/tab_3.png',
-      selectedImagePath: 'assets/fitness_app/tab_3s.png',
       index: 2,
       isSelected: false,
       animationController: null,
     ),
     TabIconData(
       icon: Icons.settings,
-      imagePath: 'assets/fitness_app/tab_4.png',
-      selectedImagePath: 'assets/fitness_app/tab_4s.png',
       index: 3,
       isSelected: false,
       animationController: null,
